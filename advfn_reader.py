5c3944e8a303775aedd95340e6e03f9e91b3c1a9
baconian <baconian@posteo.de>
2019-02-15 12:16:19 +0100

    Added start-, enddate functionality and made some cosmetic changes.

--- advfn_reader.py
+++ advfn_reader.py
@@ -1,15 +1,17 @@
-from bs4 import BeautifulSoup,SoupStrainer
+from bs4 import BeautifulSoup, SoupStrainer
 import requests
 import pandas as pd
 import datetime as dt
 from dateutil.relativedelta import relativedelta
 from multiprocessing import Pool
+import os
 
-headers = {'User-Agent':'Mozilla/5.0'}
-only_table_tags = SoupStrainer("table")
+headers = {'User-Agent': 'Mozilla/5.0'}
+only_table_tags = SoupStrainer('table')
+
 
 def get_closest_quarter(target):
-    # candidate list
+    #  candidate list
     candidates = [
         dt.date(target.year - 1, 12, 31),
         dt.date(target.year, 3, 31),
@@ -26,10 +28,11 @@ def parse_site(url):
     page = requests.get(url)
     # lxml parses slightly quicker than the html-parser
     soup = BeautifulSoup(page.text, 'lxml', parse_only=only_table_tags)
-    if soup.find_all(lambda tag: tag.name=='table' and tag.has_attr('cellpadding') and tag['cellpadding']=="2") != []:
-        table = soup.find_all(lambda tag: tag.name=='table' and tag.has_attr('cellpadding') and tag['cellpadding']=="2")[1]
+    if soup.find_all(lambda tag: tag.name == 'table' and tag.has_attr('cellpadding') and tag['cellpadding'] == '2') != []:
+        table = soup.find_all(lambda tag: tag.name == 'table' and tag.has_attr(
+            'cellpadding') and tag['cellpadding'] == '2')[1]
         table_rows = table.find_all('tr')
-        l=[]
+        l = []
         for tr in table_rows:
             td = tr.find_all('td')
             row = [j.text for j in td]
@@ -41,27 +44,43 @@ def parse_site(url):
         dl = dl.set_index('quarter end date')
         return dl
 
-def get_links(ticker,exchange):
+
+def get_links(ticker, exchange, start_date, end_date):
     """Fetches the first page of the quarterlys for a given ticker and exchange and calculates the number of pages it needs to retrieve. 
     Returns an array with the links to every fifth page. Otherwise we would be querrying duplicate information."""
-    url = "https://de.advfn.com/borse/" + exchange + "/" + ticker + "/finanzwerte?btn=istart_date&istart_date=1&mode=quarterly_reports"
+    url = 'https://de.advfn.com/borse/' + exchange + '/' + ticker + \
+        '/finanzwerte?btn=istart_date&istart_date=1&mode=quarterly_reports'
     df = parse_site(url)
-    start= dt.datetime.strptime(df.index[0], '%Y/%m')
-    end = start + relativedelta(months=3)
-    curr_date = dt.datetime.today().date()
-    num_of_pages = (relativedelta(get_closest_quarter(curr_date), start.date()).years*4 + relativedelta(get_closest_quarter(curr_date), start.date()).months)
+    start = dt.datetime.strptime(df.index[0], '%Y/%m')
+    if end_date != dt.datetime.today().date():
+        end_date = dt.datetime.strptime(end_date, '%Y/%m').date()
+    num_of_pages = (relativedelta(get_closest_quarter(end_date), start.date(
+    )).years * 4 + relativedelta(get_closest_quarter(end_date), start.date()).months / 4)
+    # If start_date if given strip it and adjust the starting point.
+    if start_date is not None:
+        start_date = dt.datetime.strptime(start_date, '%Y/%m').date()
+        i = (relativedelta(get_closest_quarter(start_date), start.date()).years *
+             4 + relativedelta(get_closest_quarter(start_date), start.date()).months / 4)
+    else:
+        i = 1
     links = []
-    i=1
-    while i< (num_of_pages):
-        links.append("https://de.advfn.com/borse/" + exchange + "/" + ticker + "/finanzwerte?btn=istart_date&istart_date=" + str(i) +"&mode=quarterly_reports")
-        i+=5
+    while i < (num_of_pages):
+        links.append('https://de.advfn.com/borse/' + exchange + '/' + ticker +
+                     '/finanzwerte?btn=istart_date&istart_date=' + str((int(i))) + '&mode=quarterly_reports')
+        i += 5
     return links
 
 
-def get_quarts(ticker, exchange = 'NYSE'):
-    """Perfoms the multiprocess parsing of a given ticker and exchange. Returns Pandas DataFrame."""
-    quart_urls = get_links(ticker, exchange)
-    with Pool(10) as p:
+def get_quarts(ticker, exchange='NYSE', start_date=None, end_date=dt.datetime.today().date()):
+    """Perfoms the multiprocess parsing of a given ticker and exchange. start_date and end_date are optional and adjust the range. Returns Pandas DataFrame all column names are lowercase."""
+    quart_urls = get_links(ticker, exchange, start_date, end_date)
+    # Creates available_threads - 2 processes if available_threads > 2.
+    # Else create available_threads
+    if len(os.sched_getaffinity(0)) > 2:
+        cpus = len(os.sched_getaffinity(0))-2
+    else:
+        cpus = len(os.sched_getaffinity(0))
+    with Pool(cpus) as p:
         records = p.map(parse_site, quart_urls)
-        result = pd.concat(records, join = 'inner')
+        result = pd.concat(records, join='inner')
     return result
